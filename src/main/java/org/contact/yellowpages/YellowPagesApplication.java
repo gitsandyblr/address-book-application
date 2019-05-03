3b918221cd11b5a71519cfc611a81bd238c46f21
sandeep <sandeepblh@gmail.com>
2019-05-03 20:12:20 +1000

    initial commit

---
+++ src/main/java/org/contact/yellowpages/YellowPagesApplication.java
@@ -0,0 +1,13 @@
+package org.contact.yellowpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowPagesApplication.class, args);
+	}
+
+}
